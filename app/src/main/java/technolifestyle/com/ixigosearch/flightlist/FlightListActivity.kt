594673395a54189008758ef7769d53678601e4e1
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-20 15:02:34 +0530

    Create instance of ViewModel on MainActivity and get flight list

--- app/src/main/java/technolifestyle/com/ixigosearch/flightlist/FlightListActivity.kt
+++ app/src/main/java/technolifestyle/com/ixigosearch/flightlist/FlightListActivity.kt
@@ -2,12 +2,20 @@ package technolifestyle.com.ixigosearch.flightlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import technolifestyle.com.ixigosearch.R
 
 class FlightListActivity : AppCompatActivity() {
 
+    private lateinit var flightListViewModel: FlightListViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flight_list)
+
+        flightListViewModel = ViewModelProviders.of(this).get(
+            FlightListViewModel::class.java)
+
+        flightListViewModel.getFlightDetails()
     }
 }
