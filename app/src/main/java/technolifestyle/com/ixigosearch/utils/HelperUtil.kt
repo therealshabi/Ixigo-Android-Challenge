61eb4cfabe7ce173cdcb6ddb53565136d0e2babd
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-20 18:52:02 +0530

    Create HelpperUtil containing time related methods

---
+++ app/src/main/java/technolifestyle/com/ixigosearch/utils/HelperUtil.kt
@@ -0,0 +1,22 @@
+package technolifestyle.com.ixigosearch.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.abs
+
+object HelperUtil {
+
+    fun getTime(timestamp: Long): String {
+        val formatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+        return formatter.format(Date(timestamp))
+    }
+
+    fun getDuration(timestampStart: Long, timestampEnd: Long): String {
+        val startDate = Date(timestampStart)
+        val endDate = Date(timestampEnd)
+        val diff = abs(endDate.time - startDate.time)
+        val diffMinutes: Long = diff / (60 * 1000) % 60
+        val diffHours: Long = diff / (60 * 60 * 1000) % 24
+        return "${diffHours}h ${diffMinutes}m"
+    }
+}
