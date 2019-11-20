7fc462e1785f144a96966758bfd0de48842b469a
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-20 12:30:22 +0530

    Create a BaseApplication class and initialized Timber logs

---
+++ app/src/main/java/technolifestyle/com/ixigosearch/BaseApplication.kt
@@ -0,0 +1,12 @@
+package technolifestyle.com.ixigosearch
+
+import android.app.Application
+import timber.log.Timber
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
