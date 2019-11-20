bce6fbd6668da6499f3c6add9e94e76c86e835d1
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-20 14:49:42 +0530

    Create a FlightAPIInterface for API endpoints

---
+++ app/src/main/java/technolifestyle/com/ixigosearch/flightlist/FlightApiInterface.kt
@@ -0,0 +1,13 @@
+package technolifestyle.com.ixigosearch.flightlist
+
+import retrofit2.Call
+import retrofit2.http.GET
+import technolifestyle.com.ixigosearch.ApiConfig.API_VERSION
+import technolifestyle.com.ixigosearch.ApiConfig.SAMPLE_FLIGHT_ENDPOINT
+import technolifestyle.com.ixigosearch.flightlist.models.FlightModel
+
+interface FlightApiInterface {
+
+    @GET("/${API_VERSION}/${SAMPLE_FLIGHT_ENDPOINT}")
+    fun getFlightDetails(): Call<FlightModel.FlightDetails>
+}
