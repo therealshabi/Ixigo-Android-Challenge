d098e583ac515e513fd5cb334089c10c3a4e8c06
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-11-20 12:41:59 +0530

    Rename MainActivity to FlightListActivity

---
+++ app/src/main/java/technolifestyle/com/ixigosearch/flightlist/FlightListActivity.kt
@@ -0,0 +1,13 @@
+package technolifestyle.com.ixigosearch.flightlist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import technolifestyle.com.ixigosearch.R
+
+class FlightListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flight_list)
+    }
+}
